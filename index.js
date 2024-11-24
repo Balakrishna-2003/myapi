bde57164653423f0297e74cacb702d6113efa2b6
Balakrishna-2003 <157281125+Balakrishna-2003@users.noreply.github.com>
2024-11-24 22:02:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-
-const app = express();
-const port = 4000;
 
 // In-memory data store
-let posts = [
+{
+"posts":[
   {
     id: 1,
     title: "The Rise of Decentralized Finance",
@@ -30,70 +26,5 @@ let posts = [
     author: "Samuel Green",
     date: "2023-08-10T09:15:00Z",
   },
-];
-
-let lastId = 3;
-
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Write your code here//
-
-//CHALLENGE 1: GET All posts
-app.get("/posts", (req, res) => {
-  res.json(posts);
-})
-
-//CHALLENGE 2: GET a specific post by id
-app.get("/posts/:id", (req, res) => {
-  console.log(req.params.id);
-  const post = posts.find((p) => p.id === parseInt(req.params.id));
-  if (!post) return res.status(404).json({ message: "Post not found" });
-  res.json(post);
-})
-
-//CHALLENGE 3: POST a new post
-app.post("/posts", (req, res) => {
-  const newId = lastId += 1;
-  const post = {
-    id: newId,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    date: new Date(),
-  }
-  lastId = newId;
-  posts.push(post);
-  res.status(201).json(post)
-})
-
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
-app.patch("/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post)=> post.id === id);
-
-  if(!post) return res.status(404).json({message: "post not found"});
-
-  if(req.body.title) post.title = req.body.title;
-  if(req.body.content) post.content = req.body.content;
-  if(req.body.author)  post.author = req.body.author;
-
-  res.json(post);
-})
-
-//CHALLENGE 5: DELETE a specific post by providing the post id.
-app.delete("/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.findIndex((post) => post.id === id);
-  if(post === -1){
-    return res.status(404).json({message: "post not found"});
-  }
-
-  posts.splice(post,1);
-  res.json({message: "post is delete successfully"})
-})
-
-app.listen(port, () => {
-  console.log(`API is running at http://localhost:${port}`);
-});
+]
+}
